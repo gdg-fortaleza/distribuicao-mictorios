c9e5a84c387bfac3d516e56bbbd80360b30eb7c2
Eduardo Borges <euduardoborges@gmail.com>
2023-02-11 10:23:44 -0300

    tentativa2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 
 
-export function Calcular_Mictorio(quantidade: number){
-  return 0;
+export function Calcular_Mictorio(quantidade: number[]){
+  let mictorios = quantidade
+
+  mictorios.map( (data: number) => {
+    if( data % 2 === 0 ){
+      return data
+    }
+    
+    return 0
+  })
+
+
 }
