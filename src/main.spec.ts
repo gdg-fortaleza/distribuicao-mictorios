05c6cccf9d1de943de42a47dfad2dc11b20e7152
Eduardo Borges <euduardoborges@gmail.com>
2023-02-11 10:32:45 -0300

    tentaiva3

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,7 +2,7 @@ import { Calcular_Mictorio } from "./main";
 
 describe('main', () => {
   it('Verificar mictorio com duas pessoas', () => {
-    expect(Calcular_Mictorio([1,2,3,4])).toBe([2,4]);
+    expect(Calcular_Mictorio([1,2,3,4])).toBe([2,4,6,8]);
   });
 });
 
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@
 
 export function Calcular_Mictorio(quantidade: number[]){
   let mictorios = quantidade
+  let posicao: number = 0;
 
   mictorios.map( (data: number) => {
     if( data % 2 === 0 ){
-      return data
+      posicao = data / 2
+      console.log(posicao)
     }
     
-    return 0
   })
 
 
