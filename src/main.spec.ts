026e7d440674484a9606ee97b48576cb6edb43a8
Eduardo Borges <euduardoborges@gmail.com>
2023-02-11 12:05:28 -0300

    Test 2

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -3,12 +3,18 @@ import { Calcular_Mictorio } from "./main";
 describe('main', () => {
   it('Verificar mictorio com duas pessoas', () => {
 
-    let posicoes = [0, 1, 0, 1]
+    let lugaresOcupados = [0,1]
 
-    let resultado = Calcular_Mictorio(posicoes);
+    let resultado = Calcular_Mictorio(2, lugaresOcupados);
 
     expect(resultado).toBe(0);
   });
+
+  it('Verificar quantidade igual a posições', () => {
+
+    let resultado = Calcular_Mictorio(3, [0,1,1]);
+    expect (resultado).toBe(0);
+  })
 });
 
 export{}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
+export function Calcular_Mictorio(quantidade: number, posicoes: number[]){
+  
+  
+  let posicoesDisponiveis: number[] = []
+  
+  if (quantidade === posicoes.length) {return 0}
 
-
-export function Calcular_Mictorio(posicoes){
-  let resultado: number[] = []
-  // let pessoasNaFila = quantidade;
-
-  posicoes.forEach( (data: number, index: number) => {
+  posicoes.forEach( (data: number, index: number) :void => {
     if (!data){
-      if (posicoes[index - 1] && posicoes[index + 1]) {
-        resultado.push(posicoes[index])
+      if (posicoes[index - 1] === 0 && posicoes[index + 1] === 0) {
+        posicoesDisponiveis.push(posicoes[index])
+        quantidade++
       }
     }
-    return resultado;
-  })
 
+    
+  } 
+  )
 
+  return posicoes
 }
